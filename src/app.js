3021a8276d506d6dcfe1b71f86ed535ca8cfa4dd
teixeirathiag0 <kbeca-thiago@hotmail.com>
2018-04-29 21:24:17 -0300

    inicio de criação das rotas

--- src/app.js
+++ src/app.js
@@ -6,44 +6,14 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
+// Carrega das rotas
+const indexRoutes = require('./routes/indexRoute');
+const productRoutes = require('./routes/productRoute');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-var object = {
-    nome: 'Thiago',
-    idade: '21',
-    profissao: 'Desenvolvedor'
-};
-
-JSON.stringify(object);
-
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        object
-    });
-});
-
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
-
-const put = router.put('/:id', (req, res, next) => {
-    let id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
-});
-
-const del = router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-});
-
-
-app.use('/', route);
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', del);
-
+app.use('/', indexRoutes);
+app.use('/products', productRoutes);
 
 module.exports = app;
