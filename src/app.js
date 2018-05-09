cd39fdebdda78f3de8c8fffad5d9568e966f27a0
teixeirathiag0 <kbeca-thiago@hotmail.com>
2018-05-09 15:47:40 -0300

    Conexão com MongoDB estabelecida

--- src/app.js
+++ src/app.js
@@ -2,9 +2,16 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+
+//Conexão com MongoDB
+mongoose.connect('mongodb://thiagoteixeira:vitriol0101@ds042379.mlab.com:42379/nodestr');
+
+//Carregando as Models
+const Product = require('./models/product');
 
 // Carrega das rotas
 const indexRoutes = require('./routes/indexRoute');
