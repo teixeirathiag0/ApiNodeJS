35da864a234690bcd70d568338ec01d590dbd0aa
teixeiragthiago <kbeca-thiago@hotmail.com>
2019-08-17 20:52:04 -0300

    adding cors

--- src/app.js
+++ src/app.js
@@ -21,7 +21,14 @@ const orderRoutes = require('./routes/orderRoute');
 const authRoutes = require('./routes/authRoute');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
 app.use('/', indexRoutes);
 app.use('/orders', orderRoutes);
